ab65ade0d30f568f11d8fca3bdbd8c450a42b87f
Jonathan Dance <jd@wuputah.com>
2009-11-25 01:17:48 -0500

    do a redirect instead for everything other than /, and cache the page

--- coming-soon.rb
+++ coming-soon.rb
@@ -2,6 +2,11 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 
-get '/*' do
+get '/' do
+  headers 'Cache-Control' => 'public, max-age=900'
   erb :index
+end
+
+get '/*' do
+  redirect '/'
 end
